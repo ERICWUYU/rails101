a8372c2789269297bd9ad50591eee2efaa6d6d3a
Andy <wuyu1989610@gmail.com>
2021-08-13 14:54:51 +0800

    people can't see edit button unless he is group owner

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -3,17 +3,20 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import 'bootstrap';
+
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap-sprockets
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
-//= require bootstrap/alert
-//= require bootstrap/dropdown
+
+
