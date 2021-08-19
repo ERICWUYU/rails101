0e58f2015ddd556ade3a60f1f7bfe3f16e2c88f0
Andy <wuyu1989610@gmail.com>
2021-08-20 00:28:10 +0800

    implement my posts

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,12 @@ Rails.application.routes.draw do
   namespace :account do
     resources :groups
   end
-  
+
+  namespace :account do
+    resources :groups
+    resources :posts
+  end
+
   root 'groups#index'
 end
 
